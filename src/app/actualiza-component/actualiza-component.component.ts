90814be530934847bdace3703b1041c5402db76d
Iván López-Baltasar <ivan.lopezbaltasar@fujitsu.com>
2024-11-28 17:28:23 +0100

    Query params

--- src/app/actualiza-component/actualiza-component.component.ts
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -12,7 +12,8 @@ export class ActualizaComponentComponent implements OnInit{
   constructor(private router: Router, private route: ActivatedRoute, private empleadosService: EmpleadosService){}
 
   ngOnInit(): void {
-    this.indice = this.route.snapshot.params['id']
+    this.accion = parseInt(this.route.snapshot.queryParams['accion']);
+    this.indice = this.route.snapshot.params['id'];
     let empleado: Empleado = this.empleadosService.encontrarEmpleado(this.indice)
     this.cuadroApellido = empleado.apellido;
     this.cuadroCargo = empleado.cargo;
@@ -26,19 +27,31 @@ export class ActualizaComponentComponent implements OnInit{
   cuadroSalario: number=0;
   empleados: Empleado[];
   indice: number;
+  accion: number;
+
+
+  // actualizaEmpleado() {
+  //   let empleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);    
+  //   this.empleadosService.actualizarEmpleado(this.indice, empleado);
+  //   this.volverHome();
+  // }
+
+  // eliminaEmpleado() {
+  //   this.empleadosService.eliminarEmpleado(this.indice);
+  //   this.volverHome();
+  // }
 
   actualizaEmpleado() {
-    let empleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);    
-    this.empleadosService.actualizarEmpleado(this.indice, empleado);
-    //this.router.navigate([''])
+    if (this.accion == 1){
+      let empleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);    
+      this.empleadosService.actualizarEmpleado(this.indice, empleado);
+    } else{
+          this.empleadosService.eliminarEmpleado(this.indice);
+    }
+
     this.volverHome();
   }
 
-  eliminaEmpleado() {
-    this.empleadosService.eliminarEmpleado(this.indice);
-    this.volverHome();
-  }
-    
   volverHome(){
     this.router.navigate([''])
   }
