991406ba69705399b7a192a9299556f56820faf6
Iván López-Baltasar <ivan.lopezbaltasar@fujitsu.com>
2024-11-28 16:19:50 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProyectosComponentComponent } from './proyectos-component/proyectos-component.component';
+import { QuienesComponentComponent } from './quienes-component/quienes-component.component';
+import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponentComponent},
+  {path:'proyectos', component: ProyectosComponentComponent},
+  {path:'quienes', component: QuienesComponentComponent},
+  {path:'contacto', component: ContactoComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
-import { EmpleadosService } from './empleados.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent  implements OnInit{
-  titulo = 'Listado de Empleados';
+export class AppComponent {
   
-  // creamos constructor para inyectar los servicios
-  constructor(private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadosService){
-    //podemos asignar el valor aqui o en onInit
-    //this.empleados = empleadosService.empleados;
-  }
-  ngOnInit(): void {
-    this.empleados = this.empleadosService.empleados;
-  }
-
-  cuadroNombre: string="";
-  cuadroApellido: string="";
-  cuadroCargo: string="";
-  cuadroSalario: number=0;
-  empleados: Empleado[];
-
-  addEmpleado() {
-    let empleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);    
-    this.empleadosService.agregarEmpleadoServicio(empleado);
-  }
   
 }
